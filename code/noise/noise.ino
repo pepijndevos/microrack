5c51615787bf0fe18c0cc8e09e01fde30c1d3c7c
Pepijn de Vos <pepijndevos@gmail.com>
2019-01-21 22:26:25 +0100

    add i2c WIP

--- code/noise/noise.ino
+++ code/noise/noise.ino
@@ -1,5 +1,8 @@
+#include "i2cslave.h"
+
 #define SER 2
 #define OE 0
+#define RCLK 5
 #define SRCLK 6
 #define SRCLR1 3
 #define SRCLR2 7
@@ -9,6 +12,7 @@
 void setup() {
   pinMode(SER, OUTPUT);
   pinMode(OE, OUTPUT);
+  pinMode(RCLK, OUTPUT);
   pinMode(SRCLK, OUTPUT);
   pinMode(SRCLR1, OUTPUT);
   pinMode(SRCLR2, OUTPUT);
@@ -21,12 +25,16 @@ void setup() {
   digitalWrite(SRCLR2, HIGH);
 
   // initialise shift register
-  tone(SRCLK, 40000);
-  delay(1);
+  //tone(SRCLK, 40000);
+  //TCCR0A = _BV(COM0A0) | _BV(COM0B1) | _BV(WGM01) | _BV(WGM00);
+  TCCR0A = _BV(COM0A0) | _BV(WGM01) | _BV(WGM00);
+  TCCR0B = _BV(WGM02) | _BV(CS00);
+  OCR0A = 20;
+  tone(RCLK, 20000);
+  delay(100);
   pinMode(SER, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
