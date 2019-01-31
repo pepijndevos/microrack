ac31c5c9f2995e17f7ced8778ded84f2a5f7b542
Pepijn de Vos <pepijndevos@gmail.com>
2019-01-31 14:33:06 +0100

    use pot for setting noise frequency

--- code/master/master.ino
+++ code/master/master.ino
@@ -6,9 +6,9 @@ void setup() {
   Serial.println("hello world");
   Wire.begin(); // join i2c bus (address optional for master)
   pinMode(3, OUTPUT);
+  pinMode(A0, INPUT);
 }
 
-int counter = 0;
 void loop() {
   struct port myport = readPort(21, 1);
   Serial.print(myport.value);
@@ -18,12 +18,12 @@ void loop() {
   Serial.println(myport.name);
   uint16_t myval = readValue(21, 1);
   Serial.println(myval);
-  writeValue(21, 1, 1<<(counter%15));  
+  uint16_t pot = analogRead(A0);
+  writeValue(21, 1, pot<<6);  
   //writeValue(21, 1, 0);  
   delay(1);
   digitalWrite(3, HIGH);
   delay(1);
   digitalWrite(3, LOW);
   delay(500);
-  counter++;
 }
